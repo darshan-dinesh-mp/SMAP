2fcb6f3bc7ad2c49e9693ab1fd1f7c2775140150
Darshan Dinesh <darshandineshmp@gmail.com>
2023-12-03 02:41:09 +0530

    created new controller to handle form input

---
+++ app/Http/Controllers/GeneralFormController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GeneralFormController extends Controller
+{
+    public function general_form(){
+        
+    }
+}
