319195d41849388af447a2cd76435e7128541e75
Delbin George <imdelbingeorge@gmail.com>
2023-12-02 20:21:02 +0530

    working on profile page

--- resources/views/student/studentprofile.blade.php
+++ resources/views/student/studentprofile.blade.php
@@ -8,47 +8,35 @@
 </head>
 
 <body class="font-poppy">
-    {{-- <div class="px-0 lg:px-12">
-        <div class="">
-            <nav class="px-4 lg:px-0 w-full flex items-center justify-between py-4">
-                <div class="-space-y-3">
-                    <h1 class="flex items-center justify-center space-x-2 text-2xl font-medium">
-                        <x-heroicon-o-arrow-small-left class="w-7 h-7" />
-                        <span>
-                            Profile
-                        </span>
-                    </h1>
-                </div>
-            </nav>
-            <div
-                class="rounded-none lg:rounded-xl bg-cover bg-[url(https://images.unsplash.com/photo-1634814516913-a0e237050d77?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)]">
-                <div class="h-48 lg:h-64 relative">
-                    <div class="absolute left-3 lg:left-12 -bottom-16 lg:-bottom-24 flex items-end space-x-3">
-                        <img class="w-28 lg:w-40 h-28 lg:h-40 object-cover rounded-xl lg:rounded-3xl border-2 lg:border-4"
-                            src="https://images.unsplash.com/photo-1517849845537-4d257902454a?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                            alt="">
-                        <div class="-space-y-1">
-                            <h1 class="font-semibold text-2xl lg:text-3xl ">Delbin George</h1>
-                            <h2 class="font-medium text-sm lg:text-lg text-black/50">nuasdlkfasljdajaslk</h2>
-                        </div>
-                    </div>
-                </div>
+    <div class="relative px-0 lg:px-12">
+        <nav class=" w-full flex items-center justify-between px-4 lg:px-0 py-4">
+            <div class="-space-y-3">
+                <h1 class="flex items-center justify-center space-x-2 text-2xl font-medium">
+                    <x-heroicon-o-arrow-small-left class="w-7 h-7" />
+                    <span>
+                        Profile
+                    </span>
+                </h1>
             </div>
-
-
-        </div>
-    </div> --}}
-    <div class="relative">
+        </nav>
         <img src="https://images.unsplash.com/photo-1634814516913-a0e237050d77?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Cover Photo" class="w-full h-56 object-cover">
-        <div class="mx-4 -mt-16 relative z-10 flex items-end">
+            alt="Cover Photo" class="w-full rounded-none lg:rounded-xl h-64 object-cover">
+        <div class="mx-3 lg:mx-8 -mt-20 relative z-10 flex items-end">
             <div class=" overflow-hidden ">
-                <img src="https://images.unsplash.com/photo-1634814516913-a0e237050d77?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    alt="Profile Picture" class="w-40 h-40 object-cover rounded-2xl">
+                <img src="https://www.delb.in/assets/DelbinGeorge-22bfff80.webp" alt="Profile Picture"
+                    class="w-40 h-40 object-cover border-4 rounded-2xl">
             </div>
             <div class="mx-4 mt-4">
-                <h1 class="text-2xl font-semibold text-gray-800">John Doe</h1>
-                <p class="text-gray-600">Web Developer</p>
+                <h1 class="text-xl lg:text-3xl font-semibold">Delbin George</h1>
+                <p class="text-md lg:text-xl text-gray-600">nnm23mc036@nmamit.in</p>
+            </div>
+        </div>
+        <div class="space-y-3 mt-4 lg:mt-8 px-4 lg:px-0">
+            <div class="space-y-1">
+                <h1 class="font-semibold tracking-widest text-black/70">ABOUT ME</h1>
+                <p class="text-lg">Passionate MCA student, coding enthusiast. Thrives
+                    on crafting elegant solutions. Constant learner
+                    embracing tech's limitless possibilities. 😋</p>
             </div>
         </div>
 </body>
