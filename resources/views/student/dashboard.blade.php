1d04e6a0b0d84aad0bab1388dfdac695ae21448c
Delbin George <imdelbingeorge@gmail.com>
2023-12-02 23:47:40 +0530

    added hamburger for studentprofile on mobile devices

--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -24,14 +24,24 @@
         </nav>
         <div class="px-4 lg:px-12 py-8">
             <h1 class="text-2xl font-medium mb-6">Latest Updates</h1>
-            <div class="grid grid-cols-1 lg:grid-cols-3 gap-6 w-full">
+            <div class="grid grid-cols-1 lg:grid-cols-3 gap-3 lg:gap-6 w-full">
+
                 <a href="{{ route('general-form') }}"
                     class="flex items-center justify-start cursor-pointer group bg-secondary space-x-4 px-6 py-7 rounded-lg duration-300">
-                    <x-heroicon-s-star
+                    <x-heroicon-o-clock
                         class="w-7 h-7 group-hover:rotate-[30deg] group-hover:text-[#ffd000] duration-300" />
-                    <h1 class="font-medium text-xl group-hover:text-dark/80">Update first MSE Marks!</h1>
+                    <h1 class="font-medium text-xl group-hover:text-dark/80">Complete your profile!</h1>
                 </a>
-
+                <div
+                    class="flex items-center justify-start cursor-not-allowed group bg-secondary space-x-4 px-6 py-7 rounded-lg duration-300">
+                    <x-heroicon-s-lock-closed class="w-7 h-7 text-black/30" />
+                    <h1 class="font-medium text-xl text-dark/30">Update first MSE Marks</h1>
+                </div>
+                <div
+                    class="flex items-center justify-start cursor-not-allowed group bg-secondary space-x-4 px-6 py-7 rounded-lg duration-300">
+                    <x-heroicon-s-lock-closed class="w-7 h-7 text-black/30" />
+                    <h1 class="font-medium text-xl text-dark/30">Update second MSE Marks</h1>
+                </div>
             </div>
         </div>
     </div>
